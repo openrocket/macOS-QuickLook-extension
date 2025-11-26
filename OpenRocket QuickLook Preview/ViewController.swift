45215e2f5e65e7a40be2e4e3e33572130509eaea
David Hoerl <dhoerl@mac.com>
2025-11-26 16:54:23 -0500

    First Commit

---
+++ OpenRocket QuickLook Preview/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OpenRocket QuickLook Preview
+//
+//  Created by David Hoerl on 11/2/25.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+}
+
